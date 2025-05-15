766e4bda2c431846e69c3fd62eaa08f0646f75f5
avarice-dev <103917021+avarice-dev@users.noreply.github.com>
2025-05-14 21:02:36 -0700

    MessageBox sample dll

---
+++ TestDLL/dllmain.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+INT __declspec(dllexport) PopMessageBox(VOID) {
+
+    MessageBoxW(NULL, L'Test Message', L'Test Title', MB_OK | MB_ICONINFORMATION);
+
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
